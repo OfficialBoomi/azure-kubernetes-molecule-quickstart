5231b8e677f5fc3e1aad8a42f1f9623db52dc233
Ganesh-Yeole <92855225+Ganesh-Yeole@users.noreply.github.com>
2022-07-07 11:14:11 +0530

    Update license_validation.sh

--- scripts/license_validation.sh
+++ scripts/license_validation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/Ganesh-Yeole/quickstart-aks-boomi-molecule/main/scripts/requirements.txt
-wget https://raw.githubusercontent.com/Ganesh-Yeole/quickstart-aks-boomi-molecule/main/scripts/license_validation.py
+wget ${REQTXT}
+wget ${LICENSEVALIDATION}
 
 pip install -t . -r ./requirements.txt
 
